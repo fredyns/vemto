3cdc29fffb6a9fa30d6a6280794ae4b1492d85df
Fredy Nurman Saleh <email@fredyns.net>
2023-12-30 10:17:23 +0700

    deployment script

---
+++ runcloud-first-deployment.sh
@@ -0,0 +1,17 @@
+# first install
+
+composer update
+composer dump-autoload -o
+npm install
+npm update
+npm run build
+php artisan key:generate
+php artisan storage:link
+
+# app codes
+
+php artisan optimize:clear
+php artisan livewire:publish --assets
+
+# reset DB
+php artisan migrate:fresh --seed # --force
