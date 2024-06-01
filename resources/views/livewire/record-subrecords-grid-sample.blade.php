9a19801fa2f29f0d35bc522fddbb652b1a0e8ebd
Fredy Nurman Saleh <email@fredyns.net>
2024-06-01 20:13:43 +0700

    fix double input

--- resources/views/livewire/record-subrecords-grid-sample.blade.php
+++ resources/views/livewire/record-subrecords-grid-sample.blade.php
@@ -2,26 +2,23 @@
     <div class="flex flex-wrap">
         <x-inputs.group class="w-full md:w-4/12 lg:w-2/12">
             <x-inputs.text
-                name="subrecord.time"
+                name="sample[]"
                 label="M4/L2"
                 placeholder="M4/L2"
-                wire:model="subrecord.time"
             ></x-inputs.text>
         </x-inputs.group>
         <x-inputs.group class="w-full md:w-4/12 lg:w-2/12">
             <x-inputs.text
-                name="subrecord.time"
+                name="sample[]"
                 label="M4/L2"
                 placeholder="M4/L2"
-                wire:model="subrecord.time"
             ></x-inputs.text>
         </x-inputs.group>
         <x-inputs.group class="w-full md:w-4/12 lg:w-2/12">
             <x-inputs.text
-                name="subrecord.time"
+                name="sample[]"
                 label="M4/L2"
                 placeholder="M4/L2"
-                wire:model="subrecord.time"
             ></x-inputs.text>
         </x-inputs.group>
     </div>
