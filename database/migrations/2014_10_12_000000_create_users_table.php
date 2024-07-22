4a257e199fb4526ab509cd3fddc01e810e253a57
Fredy Nurman Saleh <email@fredyns.net>
2024-07-22 09:38:26 +0700

    change current_team_id to uuid

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,10 @@ return new class extends Migration {
             $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
             $table->timestamp('two_factor_confirmed_at')->nullable();
-            $table->foreignId('current_team_id')->nullable();
+            $table->uuid('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
+
+            $table->index('current_team_id');
 
             $table->timestamps();
         });
