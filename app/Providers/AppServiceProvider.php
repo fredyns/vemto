b210bc2b2729d9213628f152a2c869116e3e09f3
Fredy Nurman Saleh <email@fredyns.net>
2024-09-09 14:34:05 +0700

    more records

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,5 +21,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (env('APP_ENV', 'production') != 'production') {
+            DB::listen(function ($query) {
+                Log::info(
+                    $query->sql,
+                    $query->bindings
+                );
+            });
+        }
     }
 }
