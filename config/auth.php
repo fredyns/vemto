dcd9849bcc303b7158071a5620afcb400c3a5d1f
Fredy Nurman Saleh <email@fredyns.net>
2025-01-18 00:22:49 +0700

    set superadmin from env

--- config/auth.php
+++ config/auth.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    'super_admins' => (env('APP_ENV') == 'production') ?
-        ['fredy.ns@gmail.com'] :
-        ['fredy.ns@gmail.com', 'admin@admin.com'],
+    'super_admins' => explode(',', env('SUPER_ADMINS', 'admin@admin.com')),
 
     /*
     |--------------------------------------------------------------------------
