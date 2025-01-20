2459f7da40dcf1ab8167377cae658421e22b462d
Fredy Nurman Saleh <email@fredyns.net>
2025-01-20 14:18:56 +0700

    reformating

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,98 +1,109 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Vemto') }}</title>
+    <title>{{ config('app.name', 'Vemto') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
 
-        <!-- Icons -->
-        <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <!-- Icons -->
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        @livewireStyles
+    @livewireStyles
 
-        <!-- Third Parties -->
-        <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
+    <!-- Third Parties -->
+    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
 
-        <link rel="stylesheet" href="https://unpkg.com/trix@2.0.0-alpha.1/dist/trix.css"></link>
-        <script src="https://unpkg.com/trix@2.0.0-alpha.1/dist/trix.umd.js"></script>
-        <style>[data-trix-button-group="file-tools"] { display: none !important; }</style>
+    <link rel="stylesheet" href="https://unpkg.com/trix@2.0.0-alpha.1/dist/trix.css"></link>
+    <script src="https://unpkg.com/trix@2.0.0-alpha.1/dist/trix.umd.js"></script>
+    <style>
+        [data-trix-button-group="file-tools"] {
+            display: none !important;
+        }
+    </style>
 
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+</head>
+<body class="font-sans antialiased">
+<x-banner/>
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+<div class="min-h-screen bg-gray-100">
+    @livewire('navigation-menu')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
 
-        @stack('modals')
+@stack('modals')
 
-        @livewireScripts
+@livewireScripts
 
-        @stack('scripts')
+@stack('scripts')
 
-        <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 
-        @if (session()->has('success'))
-        <script>
-            var notyf = new Notyf({dismissible: true})
-            notyf.success('{{ session('success') }}')
-        </script>
-        @endif
+<script>
+    /* simple notification */
+    var notyf = new Notyf({dismissible: true});
 
-        <script>
-            /* Simple Alpine Image Viewer */
-            document.addEventListener('alpine:init', () => {
-                Alpine.data('imageViewer', (src = '') => {
-                    return {
-                        imageUrl: src,
+    @if (session()->has('success'))
+    notyf.success('{{ session('success') }}')
+    @endif
 
-                        refreshUrl() {
-                            this.imageUrl = this.$el.getAttribute("image-url")
-                        },
+    @if (session()->has('error'))
+    notyf.error('{{ session('error') }}')
+    @endif
+</script>
 
-                        fileChosen(event) {
-                            this.fileToDataUrl(event, src => this.imageUrl = src)
-                        },
 
-                        fileToDataUrl(event, callback) {
-                            if (! event.target.files.length) return
+<script>
+    /* Simple Alpine Image Viewer */
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('imageViewer', (src = '') => {
+            return {
+                imageUrl: src,
 
-                            let file = event.target.files[0],
-                                reader = new FileReader()
+                refreshUrl() {
+                    this.imageUrl = this.$el.getAttribute("image-url")
+                },
 
-                            reader.readAsDataURL(file)
-                            reader.onload = e => callback(e.target.result)
-                        },
-                    }
-                })
-            })
-        </script>
-    </body>
+                fileChosen(event) {
+                    this.fileToDataUrl(event, src => this.imageUrl = src)
+                },
+
+                fileToDataUrl(event, callback) {
+                    if (!event.target.files.length) return
+
+                    let file = event.target.files[0],
+                        reader = new FileReader()
+
+                    reader.readAsDataURL(file)
+                    reader.onload = e => callback(e.target.result)
+                },
+            }
+        })
+    })
+</script>
+</body>
 </html>
