e3d8542d70473f2076a82d69db3769ca6ed99465
Fredy Nurman Saleh <email@fredyns.net>
2025-02-12 14:37:12 +0700

    update deployment template

--- vemto-template-plugin/files/runcloud-deployment-script.sh
+++ vemto-template-plugin/files/runcloud-deployment-script.sh
@@ -7,15 +7,14 @@ chmod -R 775 *.sh
 
 # app migrations
 
-composer dump-autoload -o
 composer update
 npm update
 npm run build
 
 # app codes
 
-php artisan optimize:clear
 php artisan livewire:publish --assets
+php artisan optimize:clear
 php artisan queue:restart
 
 # reset DB
--- vemto-template-plugin/files/runcloud-first-deployment.sh
+++ vemto-template-plugin/files/runcloud-first-deployment.sh
@@ -5,16 +5,14 @@ chmod -R 775 *.sh
 #chmod -R 775 cron
 
 composer update
-npm install
 npm update
 npm run build
 php artisan key:generate
-php artisan storage:link
 
 # app codes
 
-php artisan optimize:clear
 php artisan livewire:publish --assets
+php artisan optimize:clear
 
 # reset DB
 php artisan migrate
